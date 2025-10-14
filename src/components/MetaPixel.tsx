54ad24885a4b338a5a81740a69e296336e5353ca
autonomyowner <your-email@example.com>
2025-10-14 08:41:48 +0100

    Switch Meta Pixel to new ID

--- src/components/MetaPixel.tsx
+++ src/components/MetaPixel.tsx
@@ -4,7 +4,7 @@
 
 import Script from 'next/script'
 
-const META_PIXEL_ID = '2189573084862762'
+const META_PIXEL_ID = '752192693921476'
 
 export const MetaPixel = (): JSX.Element => (
   <>
@@ -12,9 +12,9 @@ export const MetaPixel = (): JSX.Element => (
       {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
       n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
       n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-      t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s);}
-      (window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', '${META_PIXEL_ID}');fbq('track', 'PageView');`}
+      t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+      document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init',
+      '${META_PIXEL_ID}');fbq('track','PageView');`}
     </Script>
 
     <noscript>
